0468148650c9affe8e6a05806ec9c4378e506053
BillLin1209 <zzx123bill@gmail.com>
2021-05-21 22:17:06 +0800

    setup environment

---
+++ src/main/kotlin/com/bill/kotlin/websocket/KotlinWebsocketApplication.kt
@@ -0,0 +1,11 @@
+package com.bill.kotlin.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebsocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebsocketApplication>(*args)
+}
---
+++ src/test/kotlin/com/bill/kotlin/websocket/KotlinWebsocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bill.kotlin.websocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebsocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
