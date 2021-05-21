ba8b84510ea88155bbfb6bdb181616a29e72a12b
BillLin1209 <zzx123bill@gmail.com>
2021-05-22 02:51:09 +0800

    add websocket config

---
+++ src/main/kotlin/com/bill/kotlinwebsocket/config/WebSocketConfig.kt
@@ -0,0 +1,25 @@
+package com.bill.kotlinwebsocket.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig: WebSocketMessageBrokerConfigurer {
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        super.registerStompEndpoints(registry)
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins()
+                .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        super.configureMessageBroker(registry)
+        registry.setApplicationDestinationPrefixes("/app")
+        registry.enableSimpleBroker("/topic")
+    }
+}
