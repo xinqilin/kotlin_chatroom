7949fb243aaec5931bbb29563660199a53ad952f
BillLin1209 <zzx123bill@gmail.com>
2021-05-23 20:13:51 +0800

    add websocket controller

---
+++ src/main/kotlin/com/bill/kotlinwebsocket/config/SecurityConfig.kt
@@ -0,0 +1,4 @@
+package com.bill.kotlinwebsocket.config
+
+class SecurityConfig {
+}
--- src/main/kotlin/com/bill/kotlinwebsocket/controller/ChatController.kt
+++ src/main/kotlin/com/bill/kotlinwebsocket/controller/ChatController.kt
@@ -1,4 +1,23 @@
 package com.bill.kotlinwebsocket.controller
 
+import com.bill.kotlinwebsocket.vo.ChatMessage
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.stereotype.Controller
+
+@Controller
 class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/public")
+    fun sendMessage(@Payload chatMessage: ChatMessage): ChatMessage = chatMessage
+
+    @MessageMapping("/addUser")
+    @SendTo("/topic/public")
+    fun addUser(@Payload chatMessage: ChatMessage, simpMessageHeaderAccessor: SimpMessageHeaderAccessor): ChatMessage {
+        simpMessageHeaderAccessor.sessionAttributes?.put("username", chatMessage.sender)
+        return chatMessage
+    }
 }
